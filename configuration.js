cd3f2ddbd449b60e10cc29d1d9b7a26549419bd4
typologycat <74596432+typologycat@users.noreply.github.com>
2021-06-19 21:38:09 -0700

    Add files via upload

---
+++ configuration.js
@@ -0,0 +1,539 @@
+settings = {
+    "title": "Bunny's Cognitive Functions Test",
+    "description": "This test will score each cognitive function based on your answers. The highest results will be stronger. The lowest results will be weaker.<br><br> 0 → Not At All Accurate<br>1 → Not So Accurate<br>2 → Somewhat Accurate<br>3 → Very Accurate<br>4 → Extremely Accurate",
+    "numOptions": 5
+}
+
+
+questions = [
+    {
+        "description": "I simplify concepts into easily digestible information.",
+        "weights": {
+            "Te": 4
+        }
+    },
+    {
+        "description": "I instinctively know the easiest ways to organize items or information.",
+        "weights": {
+            "Te": 4
+        }
+    },
+    {
+        "description": "Being efficient and productive comes easily to me.",
+        "weights": {
+            "Te": 4
+        }
+    },
+    {
+        "description": "I express myself firmly and bluntly.",
+        "weights": {
+            "Te": 4
+        }
+    },
+    {
+        "description": "I objectively measure whether something works or not.",
+        "weights": {
+            "Te": 4
+        }
+    },
+    {
+        "description": "I am confident in quickly making decisions or statements.",
+        "weights": {
+            "Te": 4
+
+        }
+    },
+    {
+        "description": "I am goal driven.",
+        "weights": {
+            "Te": 4
+        }
+    },
+    {
+        "description": "I feel confident delegating and managing others.",
+        "weights": {
+            "Te": 4
+        }
+    },
+    {
+       "description": "I am good at time-management.",
+        "weights": {
+            "Te": 4
+        }
+    },
+    {
+       "description": "I find structure comforting.",
+        "weights": {
+            "Te": 4
+        }
+    },
+    {
+       "description": "I am pragmatic and realistic with my goals.",
+        "weights": {
+            "Te": 4
+        }
+    },
+    {
+       "description": "I easily notice logical inconsistencies.",
+        "weights": {
+            "Ti": 4
+        }
+    },
+    {
+      "description": "I perfect and tweak concepts and theories.",
+        "weights": {
+            "Ti": 4
+        }
+    },
+    {
+      "description": "I break down concepts to test for accuracy.",
+        "weights": {
+            "Ti": 4
+        }
+    },
+    {
+      "description": "I understand things by categorizing them.",
+        "weights": {
+            "Ti": 4
+        }
+    },
+    {
+      "description": "I make sense of new information by relating it to a previously known system.",
+        "weights": {
+            "Ti": 4
+        }
+    },
+    {
+      "description": "I often make remarks on clarifications and exceptions.",
+        "weights": {
+            "Ti": 4
+        }
+    },
+    {
+      "description": "I speak with precision.",
+        "weights": {
+            "Ti": 4
+        }
+    },
+    {
+      "description": "I don’t automatically subscribe to “universal truths”; I will test them for consistency.",
+        "weights": {
+            "Ti": 4
+        }
+    },
+    {
+      "description": "I instinctively can see many sides to a problem.",
+        "weights": {
+            "Ti": 4
+        }
+    },
+    {
+      "description": "I can easily advocate for opposing positions.",
+        "weights": {
+            "Ti": 4
+        }
+    },
+    {
+      "description": "I am naturally skeptical and seek extensive validity in what I believe.",
+        "weights": {
+            "Ti": 4
+        }
+    },
+    {
+      "description": "I base my decisions on how it will affect others.",
+        "weights": {
+            "Fe": 4
+        }
+    },
+    {
+       "description": "Compromising comes easily to me.",
+        "weights": {
+            "Fe": 4
+        }
+    },
+    {
+      "description": "I instinctively know what behavior is or isn’t acceptable in different settings.",
+        "weights": {
+            "Fe": 4
+        }
+    },
+    {
+     "description": "I tailor how I act to meet the expectations and needs of those around me.",
+        "weights": {
+            "Fe": 4
+        }
+    },
+    {
+      "description": "I am aware of the impact my words and actions have on others.",
+        "weights": {
+            "Fe": 4
+        }
+    },
+    {
+       "description": "I can easily read a room.",
+        "weights": {
+            "Fe": 4
+        }
+    },
+    {
+       "description": "I feel responsible for others’ feelings.",
+        "weights": {
+            "Fe": 4
+        }
+    },
+    {
+       "description": "I can naturally read others’ desires and emotions without it being verbally expressed by them.",
+        "weights": {
+            "Fe": 4
+        }
+    },
+    {
+       "description": "I unintentionally absorb others’ emotions as my own.",
+        "weights": {
+            "Fe": 4
+        }
+    },
+    {
+       "description": "I am naturally sympathetic to the concerns of others.",
+        "weights": {
+            "Fe": 4
+        }
+    },
+    {
+       "description": "I am instinctively aware of how I am coming off to others.",
+        "weights": {
+            "Fe": 4
+        }
+    },
+    {
+       "description": "I base my decisions on an internal value system.",
+        "weights": {
+            "Fi": 4
+        }
+    },
+    {
+       "description": "I am instinctively aware of my own emotions.",
+        "weights": {
+            "Fi": 4
+        }
+    },
+    {
+       "description": "I value and seek inner emotional harmony.",
+        "weights": {
+            "Fi": 4
+        }
+    },
+    {
+       "description": "I am authentic and true to myself.",
+        "weights": {
+            "Fi": 4
+        }
+    },
+    {
+       "description": "I have a natural appreciation for life itself – especially with nature and animals.",
+        "weights": {
+            "Fi": 4
+        }
+    },
+    {
+       "description": "I quickly know how I feel about something.",
+        "weights": {
+            "Fi": 4
+        }
+    },
+    {
+       "description": "I recognize my core identity.",
+        "weights": {
+            "Fi": 4
+        }
+    },
+    {
+       "description": "I reflect on others’ emotions by relating it to my own.",
+        "weights": {
+            "Fi": 4
+        }
+    },
+    {
+       "description": "I have a strong sense of self.",
+        "weights": {
+            "Fi": 4
+        }
+    },
+    {
+       "description": "I act in accordance to my beliefs.",
+        "weights": {
+            "Fi": 4
+        }
+    },
+    {
+       "description": "I find and describe meaning in all emotions.",
+        "weights": {
+            "Fi": 4
+        }
+    },
+    {
+       "description": "I can easily connect seemingly random pieces of information.",
+        "weights": {
+            "Ne": 4
+        }
+    },
+    {
+       "description": "I instinctively see the possibilities in situations or data.",
+        "weights": {
+            "Ne": 4
+        }
+    },
+    {
+       "description": "I naturally spot patterns and connections.",
+        "weights": {
+            "Ne": 4
+        }
+    },
+    {
+       "description": "Innovation and originality comes easily to me.",
+        "weights": {
+            "Ne": 4
+        }
+    },
+    {
+       "description": "I am open minded.",
+        "weights": {
+            "Ne": 4
+        }
+    },
+    {
+       "description": "Brainstorming about various possibilities excites me.",
+        "weights": {
+            "Ne": 4
+        }
+    },
+    {
+       "description": "I am comfortable with uncertainty and ambiguity.",
+        "weights": {
+            "Ne": 4
+        }
+    },
+    {
+       "description": "I naturally see many possible implications in any scenario.",
+        "weights": {
+            "Ne": 4
+        }
+    },
+    {
+       "description": "I see things as impressions and focus on the big picture.",
+        "weights": {
+            "Ne": 4
+        }
+    },
+    {
+       "description": "I find multiple meanings from a single thing.",
+        "weights": {
+            "Ne": 4
+        }
+    },
+    {
+       "description": "I am accepting of new information.",
+        "weights": {
+            "Ne": 4
+        }
+    },
+    {
+       "description": "I easily see and prescribe meanings in everything.",
+        "weights": {
+            "Ni": 4
+        }
+    },
+    {
+       "description": "Answers naturally come to me without actively searching for them.",
+        "weights": {
+            "Ni": 4
+        }
+    },
+    {
+       "description": "I naturally know how events will most likely unfold with limited information.",
+        "weights": {
+            "Ni": 4
+        }
+    },
+    {
+       "description": "Envisioning future events comes easily to me.",
+        "weights": {
+            "Ni": 4
+        }
+    },
+    {
+       "description": "I instinctively see the implications and consequences from possible actions.",
+        "weights": {
+            "Ni": 4
+        }
+    },
+    {
+       "description": "Narrowing down possibilities to what is most probable comes easily to me.",
+        "weights": {
+            "Ni": 4
+        }
+    },
+    {
+       "description": "I communicate with metaphors and analogies.",
+        "weights": {
+            "Ni": 4
+        }
+    },
+    {
+       "description": "I see the world as a series of principles and symbolism.",
+        "weights": {
+            "Ni": 4
+        }
+    },
+    {
+       "description": "I am driven to find out why and how everything fits together and works.",
+        "weights": {
+            "Ni": 4
+        }
+    },
+    {
+       "description": "I find abstraction comforting.",
+        "weights": {
+            "Ni": 4
+        }
+    },
+    {
+       "description": "I think in fleeting blasts of insight; images and impressions rather than words.",
+        "weights": {
+            "Ni": 4
+        }
+    },
+    {
+       "description": "I am naturally present and engaging.",
+        "weights": {
+            "Se": 4
+        }
+    },
+    {
+       "description": "I am adventurous.",
+        "weights": {
+            "Se": 4
+        }
+    },
+    {
+       "description": "I respond quickly to my environment.",
+        "weights": {
+            "Se": 4
+        }
+    },
+    {
+       "description": "Being active and attentive to my surroundings comes easily to me.",
+        "weights": {
+            "Se": 4
+        }
+    },
+    {
+       "description": "I accept things as they are.",
+        "weights": {
+            "Se": 4
+        }
+    },
+    {
+       "description": "I have an eye for aesthetics.",
+        "weights": {
+            "Se": 4
+        }
+    },
+    {
+       "description": "I have a natural appreciation for my life; enjoying each moment as it comes.",
+        "weights": {
+            "Se": 4
+        }
+    },
+    {
+       "description": "I trust my impulses.",
+        "weights": {
+            "Se": 4
+        }
+    },
+    {
+       "description": "I can accurately read others’ body language.",
+        "weights": {
+            "Se": 4
+        }
+    },
+    {
+       "description": "Spontaneously enacting actions towards new opportunities comes easily to me.",
+        "weights": {
+            "Se": 4
+        }
+    },
+    {
+       "description": "I naturally learn as I go and by being hands-on.",
+        "weights": {
+            "Se": 4
+        }
+    },
+    {
+       "description": "I am detail-oriented.",
+        "weights": {
+            "Si": 4
+        }
+    },
+    {
+       "description": "I am consistent and reliable.",
+        "weights": {
+            "Si": 4
+        }
+    },
+    {
+       "description": "Routines are comforting to me.",
+        "weights": {
+            "Si": 4
+        }
+    },
+    {
+       "description": "I instinctively notice small details that others may not be aware of.",
+        "weights": {
+            "Si": 4
+        }
+    },
+    {
+       "description": "I compare what has happened in the past to the present in order to gauge what will happen next.",
+        "weights": {
+            "Si": 4
+        }
+    },
+    {
+       "description": "I am naturally patient and composed.",
+        "weights": {
+            "Si": 4
+        }
+    },
+    {
+       "description": "I am instinctively aware of how my body feels and how to remedy it.",
+        "weights": {
+            "Si": 4
+        }
+    },
+    {
+       "description": "I am nostalgic.",
+        "weights": {
+            "Si": 4
+        }
+    },
+    {
+       "description": "I complete tasks in systematic order.",
+        "weights": {
+            "Si": 4
+        }
+    },
+    {
+       "description": "I easily notice inconsistencies in the environment and others.",
+        "weights": {
+            "Si": 4
+        }
+    },
+    {
+       "description": "My memories are detailed and accurate.",
+        "weights": {
+            "Si": 4
+        }
+    },
+    {
+]
